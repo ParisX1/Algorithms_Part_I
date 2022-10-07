df03f1a946c8eafceca2529cc1d1ec727f82a1b9
Tom OMara <toma0001@student.monash.edu>
2022-10-07 15:39:02 +1100

    Percolation seems to be working.  Now need to add Stats

--- Week_01/Percolation/Percolation.java
+++ Week_01/Percolation/Percolation.java
@@ -1,27 +1,157 @@
-import edu.princeton.cs.algs4.StdOut;
+/* *****************************************************************************
+ *  Name:              Tom
+ *  Coursera User ID:  ?
+ *  Last modified:     10/7/2022
+ **************************************************************************** */
+
+// All methods assume values passed are indexed from 1.
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    public static void main(String[] args) {
-       /*
-        1. Start with a n x n grid = total_blocks
-        2. Perculation is when there is a path from the top of the grid to the bottom
-        2. States can be :
-          - open
-          - open & connected
-          - closed
-        3. Initialise all blocks to closed
-        4. Randomally select blocks to open-state
-        5. Track the number_of_iterations_performed (ie "open blocks" created)
-        5. Check if "opening" this new block has enabled perculation
-        6. Once perculation is enabled we calculate p*
+    private int gridSize;
+    private boolean[][] grid;
+    private int virtualTopSite;
+    private int virtualBottomSite;
+    private int numSitesOpen;
+    private WeightedQuickUnionUF unionFindObject;
 
-        p* = number_of_iterations_performed / total_blocks
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+        gridSize = n;
+        numSitesOpen = 0;
 
-        We re-run this process x times (millions?) as a monte-carlo simulation
-        The average of these results is our output
-       */  
+        if (gridSize <= 0)
+            throw new IllegalArgumentException("Size must be greater than zero");
+
+        grid = new boolean[gridSize][gridSize];
+
+        // Create new union find object & Set top and bottom virtual sites
+        int totalSites = gridSize * gridSize + 2; // Allow for top and bottom virtual sites
+        virtualTopSite = 0;
+        virtualBottomSite = totalSites - 1;
+        unionFindObject = new WeightedQuickUnionUF(totalSites);
+
+        // Connect top and bottom rows to virtual sites
+        int topSitetoConnect;
+        int bottomSitetoConnect;
+        for (int i = 1; i <= gridSize; i++) {
+            topSitetoConnect = xyTo1D(1, i);
+            bottomSitetoConnect = xyTo1D(gridSize, i);
+            unionFindObject.union(virtualTopSite, topSitetoConnect);
+            unionFindObject.union(virtualBottomSite, bottomSitetoConnect);
+        }
     }
 
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        validateIndices(row, col);
+        if (grid[row - 1][col - 1] == false) {
+            grid[row - 1][col - 1] = true;
+            linkSites(row, col);
+            numSitesOpen++;
+        }
+    }
 
-    
+    private void linkSites(int row, int col) {
+        // Link site left
+        int rowToLink;
+        int colToLink;
+        int baseSite = xyTo1D(row, col);
+        int linkSite;
+
+        // Link site left
+        if (col > 1) {
+            rowToLink = row;
+            colToLink = col - 1;
+            if (isOpen(rowToLink, colToLink)) {
+                linkSite = xyTo1D(rowToLink, colToLink);
+                unionFindObject.union(baseSite, linkSite);
+            }
+        }
+
+        // Link site right
+        if (col < (gridSize)) {
+            rowToLink = row;
+            colToLink = col + 1;
+            if (isOpen(rowToLink, colToLink)) {
+                linkSite = xyTo1D(rowToLink, colToLink);
+                unionFindObject.union(baseSite, linkSite);
+            }
+        }
+
+        // Link site above
+        if (row > 1) {
+            rowToLink = row - 1;
+            colToLink = col;
+            if (isOpen(rowToLink, colToLink)) {
+                linkSite = xyTo1D(rowToLink, colToLink);
+                unionFindObject.union(baseSite, linkSite);
+            }
+        }
+
+        // Link site below
+        if (row < (gridSize)) {
+            rowToLink = row + 1;
+            colToLink = col;
+            if (isOpen(rowToLink, colToLink)) {
+                linkSite = xyTo1D(rowToLink, colToLink);
+                unionFindObject.union(baseSite, linkSite);
+            }
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        validateIndices(row, col);
+        return this.grid[row - 1][col - 1];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        validateIndices(row, col);
+        int siteToCheck = xyTo1D(row, col);
+        boolean isJoined = unionFindObject.find(virtualTopSite) == unionFindObject.find(
+                siteToCheck);
+        boolean isOpen = isOpen(row, col);
+        return isJoined && isOpen;
+        // return unionFindObject.find(virtualTopSite) == unionFindObject.find(siteToCheck);
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return numSitesOpen;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return unionFindObject.find(virtualTopSite) == unionFindObject.find(virtualBottomSite);
+    }
+
+    // Take 2d row, col co-ordinates and converts to 1d index
+    private int xyTo1D(int row, int col) {
+        validateIndices(row, col);
+        row = row - 1; // Rebase to zero-index
+        col = col - 1;
+        return (row * gridSize) + col + 1; // Add one due to virtual top site
+    }
+
+    // Are row, col values valid?
+    private void validateIndices(int row, int col) {
+        if (row < 1 || row > gridSize || col < 1 || col > gridSize) {
+            throw new IllegalArgumentException(
+                    "row: " + row + "\n" +
+                            "col: " + col + "\n" +
+                            "x, y must be greater than zero and less than or equal to " + gridSize);
+        }
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+        // Percolation testPerc = new Percolation(-1);
+
+    }
+
 }
--- Week_01/Percolation/PercolationStats.java
+++ Week_01/Percolation/PercolationStats.java
@@ -1,3 +1,46 @@
+/* *****************************************************************************
+ *  Name:              Alan Turing
+ *  Coursera User ID:  123456
+ *  Last modified:     1/1/2019
+ **************************************************************************** */
+
 public class PercolationStats {
-    
+    // perform independent trials on an n-by-n grid
+    public PercolationStats(int n, int trials) {
+
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return 1.1;
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        return 1.1;
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLo() {
+        return 1.1;
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHi() {
+        return 1.1;
+    }
+
+    // test client (see below)
+    public static void main(String[] args) {
+        /*
+        Also, include a main() method that takes two command-line arguments
+        n and T, performs T independent computational experiments (discussed above)
+        on an n-by-n grid, and prints the sample mean, sample standard deviation,
+        and the 95% confidence interval for the percolation threshold. Use StdRandom
+        to generate random numbers; use StdStats to compute the sample mean and sample
+        standard deviation.
+         */
+
+    }
+
 }
