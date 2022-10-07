2c346cd447f771fa2a763cc3d4944a4272d7cefd
Tom OMara <toma0001@student.monash.edu>
2022-10-07 16:18:21 +1100

    Working on stats

--- Week_01/Percolation/PercolationStats.java
+++ Week_01/Percolation/PercolationStats.java
@@ -4,30 +4,50 @@
  *  Last modified:     1/1/2019
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class PercolationStats {
+
     // perform independent trials on an n-by-n grid
     public PercolationStats(int n, int trials) {
+        if (n < 1 || trials < 1)
+            throw new IllegalArgumentException("Grid size and trials must be one or greater.");
 
+        for (int i = 0; i < trials; i++){
+            int randRow;
+            int randCol;
+
+            Percolation testPercolation = new Percolation(n);
+            while (!testPercolation.percolates()){
+                randRow = StdRandom.uniformInt(n+1);
+                randCol = StdRandom.uniformInt(n+1);
+                if (!testPercolation.isOpen(randRow, randCol)){
+                    testPercolation.open(randRow, randCol);
+                }
+            }
+        }
     }
 
     // sample mean of percolation threshold
-    public double mean() {
-        return 1.1;
+    public double mean(int[] valuesArray) {
+        return StdStats.mean(valuesArray);
     }
 
     // sample standard deviation of percolation threshold
-    public double stddev() {
-        return 1.1;
+    public double stddev(int[] valuesArray) {
+        return StdStats.stddev(valuesArray);
     }
 
     // low endpoint of 95% confidence interval
-    public double confidenceLo() {
-        return 1.1;
+    public double confidenceLo(double mean, double stdDev, int num) {
+        return mean - ( (1.96 * stdDev) / (Math.sqrt(num)) );
     }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi() {
-        return 1.1;
+    public double confidenceHi(double mean, double stdDev, int num) {
+        return mean + ( (1.96 * stdDev) / (Math.sqrt(num)) );
     }
 
     // test client (see below)
@@ -40,6 +60,9 @@ public class PercolationStats {
         to generate random numbers; use StdStats to compute the sample mean and sample
         standard deviation.
          */
+
+        PercolationStats trailStats = new PercolationStats(10, 100);
+        System.out.println("mean /t" + trailStats.)
 
     }
 
