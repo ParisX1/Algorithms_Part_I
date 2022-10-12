280e06b24b17cb3d7112914cc0705a13d83fd196
Tom OMara <toma0001@student.monash.edu>
2022-10-12 11:09:53 +1100

    Changing Deque to linked list

---
+++ Week_02/Queues/src/Deque-old.java
@@ -0,0 +1,158 @@
+/*
+ * Dequeue. A double-ended queue or deque (pronounced "deck") is a generalization of a stack 
+ * and a queue that supports adding and removing items from either the front or the back of the 
+ * data structure. Create a generic data type Deque that implements the following API.
+   https://coursera.cs.princeton.edu/algs4/assignments/queues/specification.php
+ */
+
+// The other exceptions you should throw are:
+// java.lang.IllegalArgumentException 
+// java.lang.UnsupportedOperationException,
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+// public class Deque {
+public class Deque<Item> implements Iterable<Item> {
+    
+    private Item[] deque;
+    private final int INIT_CAPACITY = 8;
+    private int dequeCapacity;
+    private int dequeObjectCount;
+    private int dequeHead;
+    private int dequeTail;
+
+    // construct an empty deque
+    public Deque(){
+        dequeCapacity = INIT_CAPACITY;
+        dequeObjectCount = 0;
+        deque = (Item[]) new Object[dequeCapacity];
+
+    }
+
+    // is the deque empty?
+    public boolean isEmpty() {
+        return dequeObjectCount == 0;
+    }
+
+    // return the number of items on the deque
+    public int size() {
+        int i = 0;
+        int count = 0;
+        while (i < dequeCapacity){
+            if (deque[i] != null)
+                count++;
+            i++;
+        }
+        return count;
+    }
+
+    // add the item to the front
+    public void addFirst(Item item) {
+        
+        if (item == null)
+            throw new IllegalArgumentException();
+        
+        if (dequeHead != 0){
+            deque[--dequeHead] = item;
+            dequeObjectCount++;
+        }
+    }
+
+    // add the item to the back
+    public void addLast(Item item) {
+
+        if (item == null)
+            throw new IllegalArgumentException();
+
+        if (dequeTail < dequeCapacity){ // WHAT IS THIS ACTUALLY DOING???
+            deque[dequeTail] = item;
+            dequeObjectCount++;
+        }
+    }
+
+    // remove and return the item from the front
+    public Item removeFirst() {
+        
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        Item returnItem = (Item) new Object();
+        returnItem = deque[dequeHead];
+        deque[dequeHead] = null;
+        dequeObjectCount--;
+        dequeHead++;
+
+        checkForResize();
+
+        return returnItem;
+    }
+
+    // remove and return the item from the back
+    public Item removeLast() {
+        
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        Item returnItem = (Item) new Object();
+        returnItem = deque[dequeTail];
+        deque[dequeTail] = null;
+        dequeObjectCount--;
+        dequeTail--;
+        return returnItem;
+    }
+
+    private void checkForResize(){
+        
+        // Resize if array too large
+        if (dequeObjectCount == (dequeCapacity / 4)){
+            resizeDeque(dequeCapacity / 2);
+        }
+    }
+
+    private void resizeDeque(int newCapacity){
+        Item[] dequeCopy = (Item[]) new Object[newCapacity];
+        int j = 0;
+        for (int i = 0; i < dequeCapacity; i++){
+            if (deque[i] != null){
+                dequeCopy[j] = deque[i];
+                j++;
+            }
+        }
+        deque = dequeCopy;
+        dequeCapacity = newCapacity;
+    }
+
+    // return an iterator over items in order from front to back
+    public Iterator<Item> iterator() {
+        return new ReverseArrayIterator();
+    }
+
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int i = dequeTail; 
+    
+        public boolean hasNext() { 
+            return i > 0; 
+        }
+        
+        public void remove() { 
+            /* not supported */ 
+            // Throw some error or message?
+            throw new UnsupportedOperationException();
+        }
+        
+        public Item next() { 
+            if ( (dequeHead == 0) || (deque[dequeHead - 1] == null) )
+                throw new NoSuchElementException();
+            else 
+                return deque[--i]; 
+        }
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        
+    }
+
+}
--- Week_02/Queues/src/Deque.java
+++ Week_02/Queues/src/Deque.java
@@ -1,125 +1,79 @@
 /*
- * Dequeue. A double-ended queue or deque (pronounced “deck”) is a generalization of a stack 
+ * Dequeue. A double-ended queue or deque (pronounced "deck") is a generalization of a stack 
  * and a queue that supports adding and removing items from either the front or the back of the 
  * data structure. Create a generic data type Deque that implements the following API.
    https://coursera.cs.princeton.edu/algs4/assignments/queues/specification.php
  */
 
-// The other exceptions you should throw are:
-// java.lang.IllegalArgumentException 
-// java.lang.UnsupportedOperationException,
-
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
-// public class Deque {
-public class Deque<Item> implements Iterable<Item> {
+public class Deque {
+
+    private Node first = null;
+    private Node last = null;
+    private int dequeSize;
     
-    private Item[] deque;
-    private int dequeCapacity;
-    private int dequeObjectCount;
-    private int dequeHead;
-    private int dequeTail;
 
     // construct an empty deque
-    public Deque(){
-        dequeCapacity = 0;
-        dequeObjectCount = 0;
-        deque = (Item[]) new Object[dequeCapacity];
+    public Deque() {
+
+    }
+
+    private class Node {
+        Object item;
+        Node next;
+        
+        // construct a Node
+        public Node() {
+
+        }
 
     }
 
     // is the deque empty?
     public boolean isEmpty() {
-        return dequeObjectCount == 0;
+        return first == null;
     }
 
     // return the number of items on the deque
     public int size() {
-        int i = 0;
-        int count = 0;
-        while (i < dequeCapacity){
-            if (deque[i] != null)
-                count++;
-            i++;
-        }
-        return count;
+        return dequeSize;
     }
 
     // add the item to the front
     public void addFirst(Item item) {
-        if (dequeHead != 0){
-            deque[--dequeHead] = item;
-            dequeObjectCount++;
-        }
+        Node oldfirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldfirst;
+        dequeSize++;
     }
 
     // add the item to the back
     public void addLast(Item item) {
-        if (dequeTail < dequeCapacity){ // WHAT IS THIS ACTUALLY DOING???
-            deque[dequeTail] = item;
-            dequeObjectCount++;
-        }
+        
     }
 
     // remove and return the item from the front
     public Item removeFirst() {
-        /*
-        // if(!isEmpty()){
-        // }
-        Item tempItem = (Item) new Object();
-        // tempItem = (Item) new Object();
-        tempItem = deque[dequeHead];
-        dequeHead++;
-        dequeObjectCount--;
-        return tempItem;
-         */
-        dequeObjectCount--;
-        return deque[dequeHead++];
+        Object item = first.item;
+        first = first.next;
+        return item;
+        dequeSize--;
     }
 
     // remove and return the item from the back
     public Item removeLast() {
-        dequeObjectCount--;
-        return deque[dequeTail--];
-        
+        Object item = last.item;
+        last = last.next;
+        return item;
+        dequeSize--;
     }
 
     // return an iterator over items in order from front to back
     public Iterator<Item> iterator() {
-        return new ReverseArrayIterator();
-    }
-    
-    // private class ListIterator implements Iterator<Item> {
-    //     private Node current = first;
-    //     public boolean hasNext() { return current != null; }
-    //     public void remove() { /* not supported */ }
-    //     public Item next() {
-    //         Item item = current.item;
-    //         current = current.next;
-    //         return item;
-    //     }
-    // }
-
-    private class ReverseArrayIterator implements Iterator<Item> {
-        private int i = dequeTail; 
-    
-        public boolean hasNext() { 
-            return i > 0; 
-        }
         
-        public void remove() { 
-            /* not supported */ 
-            // Throw some error or message?
-            throw new UnsupportedOperationException();
-        }
-        
-        public Item next() { 
-            if ( (dequeHead == 0) || (deque[dequeHead - 1] == null) )
-                throw new NoSuchElementException();
-            else 
-                return deque[--i]; 
-        }
     }
 
     // unit testing (required)
