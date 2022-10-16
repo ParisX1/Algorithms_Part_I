9a081a6656012af79c38a4869ff23f16b8b111d8
Tom OMara <toma0001@student.monash.edu>
2022-10-17 10:02:29 +1100

    Add shell files for Week 03 Programming assignment

---
+++ Week_03/Collinear_Points/src/FastCollinearPoints.java
@@ -0,0 +1,51 @@
+/*
+ The method segments() should include each maximal line segment containing 4 (or more) 
+ points exactly once. For example, if 5 points appear on a line segment in the order 
+ p→q→r→s→t, then do not include the subsegments p→s or q→t.
+ */
+
+import edu.princeton.cs.algs4.StdDraw;
+
+public class FastCollinearPoints {
+    public FastCollinearPoints(Point[] points) {
+        // finds all line segments containing 4 or more points
+    }
+
+    public           int numberOfSegments() {
+        // the number of line segments
+    }
+
+    public LineSegment[] segments() {
+        // the line segments
+    }
+}
+
+public static void main(String[] args) {
+
+    // read the n points from a file
+    In in = new In(args[0]);
+    int n = in.readInt();
+    Point[] points = new Point[n];
+    for (int i = 0; i < n; i++) {
+        int x = in.readInt();
+        int y = in.readInt();
+        points[i] = new Point(x, y);
+    }
+
+    // draw the points
+    StdDraw.enableDoubleBuffering();
+    StdDraw.setXscale(0, 32768);
+    StdDraw.setYscale(0, 32768);
+    for (Point p : points) {
+        p.draw();
+    }
+    StdDraw.show();
+
+    // print and draw the line segments
+    FastCollinearPoints collinear = new FastCollinearPoints(points);
+    for (LineSegment segment : collinear.segments()) {
+        StdOut.println(segment);
+        segment.draw();
+    }
+    StdDraw.show();
+}
