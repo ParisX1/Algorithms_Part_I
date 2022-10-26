bedc68dd38a65aa13ceb89b9c7b709212c6794a1
Tom OMara <toma0001@student.monash.edu>
2022-10-26 17:51:35 +1100

    Add Week 4 Board.java
    Utility classes implement and major methods tested.

--- Week_04/SliderPuzzle/src/Board.java
+++ Week_04/SliderPuzzle/src/Board.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.In;
+import java.util.ArrayList;
+
 public class Board {
 
     /* The problem. 
@@ -11,13 +15,23 @@ public class Board {
 
     // create a board from an n-by-n array of tiles,
     // where tiles[row][col] = tile at (row, col)
+    
+    private int sliderBoard[][];
+    private int boardSize;
+    
     public Board(int[][] tiles) {
         /* Constructor.  
         * You may assume that the constructor receives an n-by-n array containing 
         * the n2 integers between 0 and n2 − 1, where 0 represents the blank square. 
         * You may also assume that 2 ≤ n < 128.
          */
-
+        boardSize = tiles.length;
+        sliderBoard = new int[boardSize][boardSize];
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                sliderBoard[i][j] = tiles[i][j];
+            }
+        }
     }
                                            
     // string representation of this board
@@ -29,10 +43,10 @@ public class Board {
          * designate the blank square.
          */
         StringBuilder s = new StringBuilder();
-        s.append(n + "\n");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                s.append(String.format("%2d ", tiles[i][j]));
+        s.append(boardSize + "\n");
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                s.append(String.format("%2d ", sliderBoard[i][j]));
             }
             s.append("\n");
         }
@@ -41,7 +55,7 @@ public class Board {
 
     // board dimension n
     public int dimension() {
-
+        return boardSize;
     }
 
     // number of tiles out of place
@@ -49,7 +63,16 @@ public class Board {
         /*The Hamming distance betweeen a board and the goal board is the number of 
         * tiles in the wrong position.
         */
+        int countWrongPositions = 0;
+        int currentCorrectNumber = 1;
 
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (sliderBoard[i][j] != currentCorrectNumber) countWrongPositions++;
+                currentCorrectNumber++;
+            }
+        }
+        return countWrongPositions;
     }
 
     // sum of Manhattan distances between tiles and goal
@@ -58,12 +81,28 @@ public class Board {
         * the Manhattan distances (sum of the vertical and horizontal distance) 
         * from the tiles to their goal positions. 
         */
-
+        int cumulativeManhattan = 0;
+        int targetNumber = 1;
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (sliderBoard[i][j] != targetNumber)  cumulativeManhattan += getManhattanDistance(i, j, targetNumber);
+                targetNumber++;
+            }
+        }
+        return cumulativeManhattan;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
+        int currentCorrectNumber = 1;
 
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (sliderBoard[i][j] != currentCorrectNumber) return false;
+                currentCorrectNumber++;
+            }
+        }
+        return true;
     }
 
     // does this board equal y?
@@ -73,7 +112,20 @@ public class Board {
         * tiles are in the same positions. The equals() method is inherited from 
         * java.lang.Object, so it must obey all of Java’s requirements.
         */
+        Board that = (Board) y;
 
+        if (y == this) return true;
+        if (y == null) return false;
+        if (y.getClass() != this.getClass()) return false;
+        if (that.dimension() != this.dimension()) return false;
+
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (this.getXYValue(i, j) != that.getXYValue(i, j)) return false;
+                // if (sliderBoard[i][j] != that[i][j]) return false;
+            }
+        }
+        return true;
     }
 
     // all neighboring boards
@@ -83,12 +135,107 @@ public class Board {
         * the neighbors of the board. Depending on the location of the blank square, 
         * a board can have 2, 3, or 4 neighbors.
         */
+        // int numNeighbors;
+        int tempValue;
+        int size = this.dimension();
+        int tempBoard[][] = new int[size][size];
+        int zeroX = 0;
+        int zeroY = 0;   // x, y position of zero tile
+        int swapX, swapY;
+        ArrayList<Board> neighborsList = new ArrayList<Board>();
 
+        // Find location of zero tile
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (this.getXYValue(i, j) == 0) {
+                    zeroX = i;
+                    zeroY = j;
+                }
+            }
+        }
+
+        /*
+        // Set number of neighbours
+        if (((zeroX == 0) || (zeroX == size-1)) && ((zeroY == 0) || (zeroY == size-1))) numNeighbors = 2;       // Zero tile in corner
+        else if (((zeroX == 0) || (zeroX == size-1)) || ((zeroY == 0) || (zeroY == size-1))) numNeighbors = 3;  // Zero tile on edge row or colum, but not corner
+        else numNeighbors = 4;                                                                                  // Zero tile off edge rows and columns ("Somewhere in the middle")
+        */
+
+        for (int i = -1; i <= 1; i++){
+            for (int j = -1; j <= 1; j++){
+                swapX = zeroX + i;
+                swapY = zeroY + j;
+                if ((swapX >= 0 && swapX < size) &&                     // Check outside x bounds
+                    (swapY >= 0 && swapY < size) &&                     // Check outside y bounds
+                    //(swapX != zeroX && swapY != zeroY) &&              // Check not at the zero position
+                    (Math.abs(i) != Math.abs(j)))                       // Check not on diagonals
+                    {               
+                    tempValue = this.getXYValue(swapX, swapY);          // Save value to swap
+                    tempBoard = copyBoardArray(this);                   // Make copy of board
+                    Board newBoard = new Board(tempBoard);              // Create new board object
+                    newBoard.changeXYValue(swapX, swapY, 0);  //  
+                    newBoard.changeXYValue(zeroX, zeroY, tempValue);    // Swap target tile with zero tile
+                    neighborsList.add(newBoard);                        // Add board to ArrayList
+                }
+            }
+        }
+        return neighborsList;
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
+        Board newBoard = new Board(this.sliderBoard);
+        boolean found = false;
+        int x1, y1, x2, y2, val1, val2;
 
+        while (found == false) {
+
+            x1 = StdRandom.uniformInt(boardSize);
+            x2 = StdRandom.uniformInt(boardSize);
+            y1 = StdRandom.uniformInt(boardSize);
+            y2 = StdRandom.uniformInt(boardSize);
+
+            if (!(x1 == x2 && y1 == y2) &&          // No self-swap
+                (this.getXYValue(x1, y1) != 0) &&   //
+                (this.getXYValue(x2, y2) != 0))     // No zero-values
+                {
+                val1 = this.getXYValue(x1, y1);
+                val2 = this.getXYValue(x2, y2);
+                newBoard.changeXYValue(x1, y1, val2);
+                newBoard.changeXYValue(x2, y2, val1);
+                found = true;
+            }
+        }
+        return newBoard;
+    }
+
+    private int getXYValue(int x, int y){
+        return sliderBoard[x][y];
+    }
+
+    private void changeXYValue(int x, int y, int newValue){
+        this.sliderBoard[x][y] = newValue;
+    }
+
+    private int getManhattanDistance(int x, int y, int targetNumber){
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                if (sliderBoard[i][j] == targetNumber) return (Math.abs(x - i) + Math.abs(y - j));
+            }
+        }
+        return 0; // Not found -> null (zero) tile
+    }
+
+    private int[][] copyBoardArray(Board sourceBoard){
+        int boardSize = sourceBoard.dimension();
+        int newBoard[][] = new int[boardSize][boardSize];
+
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                newBoard[i][j] = sourceBoard.getXYValue(i, j);
+            }
+        }
+        return newBoard;
     }
 
     // unit testing (not graded)
@@ -98,5 +245,43 @@ public class Board {
         * and help verify that they works as prescribed (e.g., by printing results 
         * to standard output).
          */
+        //for (String filename : args) {
+            String filename = "tests/puzzle04.txt";
+            In in = new In(filename);
+            int n = in.readInt();
+            int[][] tiles = new int[n][n];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    tiles[i][j] = in.readInt();
+                }
+            }
+            Board initial = new Board(tiles);
+            System.out.println(initial.toString());
+
+            // Test: Neighbours
+            System.out.println("~Testing Neighbors~");
+            for (Board boardInList : initial.neighbors()){
+                System.out.println(boardInList.toString());
+            }
+
+            // Test: Twins
+            System.out.println("~Testing Twins~");
+            System.out.println("Base Board~");
+            System.out.println(initial.toString());
+            System.out.println("Twins~");
+            Board twin01 = initial.twin();
+            Board twin02 = initial.twin();
+            Board twin03 = initial.twin();
+            System.out.println(twin01.toString());
+            System.out.println(twin02.toString());
+            System.out.println(twin03.toString());
+
+            // Test: Manhattan
+            System.out.println("~Testing Manhattan~");
+            System.out.print("Output: " + initial.manhattan() + "\n");
+            System.out.println("Expected: 4");
+
+        //}
+        
     }
 }
